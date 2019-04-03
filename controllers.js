88763a96b4d8ab6b9431eaed498c94a8bd6f5068
Viwe Somagaca <viwe@fonk.capetown>
2019-04-03 12:00:26 +0200

    Added routes inside the controllers

--- controllers.js
+++ controllers.js
@@ -1,6 +1,6 @@
 const item = require('./models');
 
-const getItem = (req,res);
+const getItem = (req,res) => {
  item.find()
  .then((data) =>{
     console.log(data)
@@ -9,7 +9,21 @@ const getItem = (req,res);
 .catch(err => {
     if(err) return res.json({ success: false, error: err });
   });
+};
+
+const postItem = (req,res) =>{
+    const item = new item();
+    item.itemname = req.body.itemname;
+
+    item.save(err =>{
+        return res.json({status: true});
+    })
+
+}
 
   module.exports = {
       getItem,
+      
   }
+
+ 
--- routes.js
+++ routes.js
@@ -3,8 +3,11 @@ const emesa = require("./controllers");
 
 const router = express.Router();
 
-router.get('/items', emesa.getItem)
-       .post()
+router.get('/items', emesa.getItem);
+router.post('/items', emsa.postItems);
+
+router.delete('/items:id', emesa.deleteIte);
+router.update('/items:id', emesa.updateItem);
 
 
 module.exports = router;
